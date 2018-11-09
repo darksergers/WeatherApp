a9dc322b7bc86381d6feed96d99d0b2224eb23a6
Serg Kalinin <kalinin.s@digdes.com>
2018-11-09 17:09:25 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Serg Kalinin on 09/11/2018.
+//  Copyright © 2018 Serg Kalinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
